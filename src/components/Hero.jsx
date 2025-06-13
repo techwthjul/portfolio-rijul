17b87d36808c262cd4c6e71216498ca7381b862c
Rijul Ugawekar <rnugawek@syr.edu>
2025-06-13 17:42:37 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Rijul</span>
+            Hi, I'm <span className='text-[#915EFF]'>Rijul Ugawekar</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I build data-driven products, <br className='sm:block hidden' />
