fb9ca8d598288362d40d5de68d0b5e49121c5236
Rijul Ugawekar <rnugawek@syr.edu>
2025-06-13 19:32:51 -0400

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // src/App.jsx
 
-//... imports
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
+    // This line requires the import from "react-router-dom"
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
@@ -12,8 +14,7 @@ const App = () => {
         </div>
         <About />
         <Experience />
-        {/* Temporarily comment this out to reduce the number of canvases */}
-        {/* <Tech /> */}
+        <Tech />
         <Works />
         <Feedbacks />
         <div className='relative z-0'>
