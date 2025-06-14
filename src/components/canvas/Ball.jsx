e397b26825253686ffe2713a963fa1de755c7510
Rijul Ugawekar <rnugawek@syr.edu>
2025-06-14 00:43:24 -0400

    m

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,5 +1,14 @@
-import React from "react";
-import { Decal, Float, useTexture } from "@react-three/drei";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
@@ -28,4 +37,21 @@ const Ball = (props) => {
   );
 };
 
-export default Ball;
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
